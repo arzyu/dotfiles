0d01367ce57d503ecd2fc60c4ad930d90e033936
arzyu <arzyu@live.cn>
2017-01-11 22:54:53 +0800

    add taobao mirror for nvm and nodejs

---
+++ nodejs/path.zsh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## use taobao mirror for nodejs and packages
+export NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
+export PHANTOMJS_CDNURL='https://npm.taobao.org/dist/phantomjs'
+export SASS_BINARY_SITE='https://npm.taobao.org/mirrors/node-sass'
+
---
+++ nvm/path.zsh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+## use taobao mirror
+export NVM_NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
+
