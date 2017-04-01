321fc7f23871171ceb2fbe9341a5af3f6217888b
arzyu <arzyu@live.cn>
2017-04-01 15:17:41 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 DOTFILES_ROOT="$(cd "$(dirname "$0")"; pwd)"
 cd $HOME
-find $DOTFILES_ROOT -path "./.git" -prune -or -name "*.link" -print \
+
+echo "=> Make global symbol links:"
+find $DOTFILES_ROOT -regex ".*/\.git$" -prune -or -name "*.link" -print \
   | xargs -I@ sh -c 'ln -svf @ .$(basename @ .link)'
+
+echo -e "\n=> Execute individual setup scripts:"
+find $DOTFILES_ROOT -regex ".*/.git$" -prune -or -path "$DOTFILES_ROOT/*/setup.sh" -exec "{}" ";"
 
