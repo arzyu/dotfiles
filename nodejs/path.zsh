97dd0b9df580e410133e171542829df69f077558
arzyu <arzyu@live.cn>
2017-04-05 21:05:33 +0800

    add package mirror for electron

--- nodejs/path.zsh
+++ nodejs/path.zsh
@@ -4,4 +4,5 @@
 export NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
 export PHANTOMJS_CDNURL='https://npm.taobao.org/dist/phantomjs'
 export SASS_BINARY_SITE='https://npm.taobao.org/mirrors/node-sass'
+export ELECTRON_MIRROR='https://npm.taobao.org/mirrors/electron/'
 
