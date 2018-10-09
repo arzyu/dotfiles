704bb7bad5be6c2c2f07ab746b511bbe7613d0d5
arzyu <arzyu@live.cn>
2018-10-09 12:02:10 +0800

    update android path

--- android/path.zsh
+++ android/path.zsh
@@ -2,3 +2,4 @@
 
 export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
 
+export PATH=$ANDROID_SDK_ROOT/build-tools/$(ls $ANDROID_SDK_ROOT/build-tools | sort | tail -1):$ANDROID_SDK_ROOT/platform-tools:$PATH
