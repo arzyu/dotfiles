c47b0f207e9930ad2e71744a442ecc7bf1282c1d
arzyu <arzyu@live.cn>
2018-10-09 12:04:00 +0800

    update zsh binding keys

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,7 +8,10 @@ export EDITOR='vim'
 
 # use vi mode and bindkey
 bindkey -v
-bindkey '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-pattern-search-backward
+bindkey -M vicmd '^f' history-incremental-pattern-search-forward
+bindkey -M viins '^r' history-incremental-pattern-search-backward
+bindkey -M viins '^f' history-incremental-pattern-search-forward
 bindkey '^p' up-line-or-search
 bindkey '^n' down-line-or-search
 
