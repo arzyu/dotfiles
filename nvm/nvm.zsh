7d0a9c7ff35e49736c9da3ba1af71f48e98ac763
arzyu <arzyu@live.cn>
2019-08-22 23:24:41 +0800

    update nvm config

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,3 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
+
 [[ -s "/usr/local/opt/nvm/nvm.sh" ]] && source "/usr/local/opt/nvm/nvm.sh"
 
+## set nodejs mirror: taobao
+export NVM_NODEJS_ORG_MIRROR="https://npm.taobao.org/mirrors/node"
--- nvm/path.zsh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-## use taobao mirror
-export NVM_NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
-
