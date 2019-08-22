2e895fd7961f39d2df6eeeaa41c55d7063c2cdb1
arzyu <arzyu@live.cn>
2019-08-22 23:27:47 +0800

    remove nodejs config

--- nodejs/path.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## use taobao mirror for nodejs and packages
-export NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
-export PHANTOMJS_CDNURL='https://npm.taobao.org/dist/phantomjs'
-export SASS_BINARY_SITE='https://npm.taobao.org/mirrors/node-sass'
-export ELECTRON_MIRROR='https://npm.taobao.org/mirrors/electron/'
-
