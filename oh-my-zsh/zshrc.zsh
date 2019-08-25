78e04906b712a228300afb733edb07c97cc35845
arzyu <arzyu@live.cn>
2019-08-25 16:59:44 +0800

    remove nvm

--- oh-my-zsh/zshrc.zsh
+++ oh-my-zsh/zshrc.zsh
@@ -51,7 +51,7 @@ ZSH_THEME="nicoulaj"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(nvm npm yarn pip gem bundler docker docker-compose adb)
+plugins=(npm yarn pip gem bundler docker docker-compose adb)
 
 source $ZSH/oh-my-zsh.sh
 
