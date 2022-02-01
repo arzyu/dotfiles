869045eb71be0e6041ac7576de8d7dc46147a120
arzyu <arzyu@live.cn>
2022-02-01 20:58:23 +0800

    refactor dotfiles

--- android/path.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
-
-export PATH=$PATH:$ANDROID_SDK_ROOT/build-tools/$(ls $ANDROID_SDK_ROOT/build-tools | sort | tail -1):$ANDROID_SDK_ROOT/platform-tools
--- link.sh
+++ link.sh
@@ -3,10 +3,5 @@
 DOTFILES_ROOT="$(cd "$(dirname "$0")"; pwd)"
 cd $HOME
 
-echo "=> Make global symbol links:"
 find $DOTFILES_ROOT -regex ".*/\.git$" -prune -or -name "*.link" -print \
   | xargs -I@ sh -c 'ln -svf @ .$(basename @ .link)'
-
-echo -e "\n=> Execute individual setup scripts:"
-find $DOTFILES_ROOT -regex ".*/.git$" -prune -or -path "$DOTFILES_ROOT/*/setup.sh" -exec "{}" ";"
-
